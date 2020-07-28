89f000fb581591cf3d5fc3354cec4b443bcfbb71
KJeff01 <kjeff1997@gmail.com>
2020-07-28 13:20:56 -0500

    Fix another rebase issue with New Paradigm research.

--- script/campaign/cam1-7.js
+++ script/campaign/cam1-7.js
@@ -263,7 +263,7 @@ function eventStartLevel()
 		"artifactLocation": { tech: ["R-Wpn-Cannon3Mk1", "R-Wpn-RocketSlow-Damage03"] },
 	});
 
-	camCompleteRequiredResearch(NEW_PARADIGM_RESEARCH, NEW_PARADIGM);
+	camCompleteRequiredResearch(NEW_PARADIGM_RES, NEW_PARADIGM);
 	camCompleteRequiredResearch(SCAVENGER_RES, SCAV_7);
 
 	camUpgradeOnMapTemplates(cTempl.bloke, cTempl.blokeheavy, SCAV_7);

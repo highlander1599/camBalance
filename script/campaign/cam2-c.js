929a42a3b07af6e5c063888d894d1772c756c5bb
alfred007 <highlander1599@gmail.com>
2020-08-31 12:46:04 +0200

    Update cam2-c.js
    Removing the unnecessary Cyborg and VTOL Production upgrades

--- script/campaign/cam2-c.js
+++ script/campaign/cam2-c.js
@@ -9,17 +9,14 @@ var lastSoundTime; //Only play the "civilian rescued" sound every so often.
 const COLLECTIVE_RES = [
 	"R-Defense-WallUpgrade06", "R-Struc-Materials06", "R-Struc-Factory-Upgrade06",
 	"R-Vehicle-Engine04", "R-Vehicle-Metals05", "R-Cyborg-Metals05",
-	"R-Wpn-Cannon-Accuracy02", "R-Wpn-Cannon-Damage05",
-	"R-Wpn-Cannon-ROF01", "R-Wpn-Flamer-Damage06", "R-Wpn-Flamer-ROF03",
-	"R-Wpn-MG-Damage06", "R-Wpn-MG-ROF02", "R-Wpn-Mortar-Acc02",
-	"R-Wpn-Mortar-Damage03", "R-Wpn-Mortar-ROF01",
-	"R-Wpn-Rocket-Accuracy02", "R-Wpn-Rocket-Damage05",
-	"R-Wpn-Rocket-ROF03", "R-Wpn-RocketSlow-Accuracy03",
-	"R-Wpn-RocketSlow-Damage05", "R-Sys-Sensor-Upgrade01", "R-Wpn-RocketSlow-ROF01",
-	"R-Wpn-Howitzer-ROF01", "R-Wpn-Howitzer-Damage01", "R-Cyborg-Armor-Heat01",
-	"R-Vehicle-Armor-Heat01", "R-Wpn-Bomb-Accuracy01", "R-Struc-VTOLFactory-Upgrade03",
-	"R-Struc-Factory-Cyborg-Upgrade06", "R-Wpn-AAGun-Damage03", "R-Wpn-AAGun-ROF02",
-	"R-Wpn-AAGun-Accuracy01"
+	"R-Wpn-Cannon-Accuracy02", "R-Wpn-Cannon-Damage05","R-Wpn-Cannon-ROF01",
+	"R-Wpn-Flamer-Damage06", "R-Wpn-Flamer-ROF03", "R-Wpn-MG-Damage06",
+	"R-Wpn-MG-ROF02", "R-Wpn-Mortar-Acc02", "R-Wpn-Mortar-Damage03",
+	"R-Wpn-Mortar-ROF01", "R-Wpn-Rocket-Accuracy02", "R-Wpn-Rocket-Damage05",
+	"R-Wpn-Rocket-ROF03", "R-Wpn-RocketSlow-Accuracy03", "R-Wpn-RocketSlow-Damage05",
+	"R-Sys-Sensor-Upgrade01", "R-Wpn-RocketSlow-ROF01", "R-Wpn-Howitzer-ROF01",
+	"R-Wpn-Howitzer-Damage01", "R-Cyborg-Armor-Heat01", "R-Vehicle-Armor-Heat01",
+	"R-Wpn-Bomb-Accuracy01", "R-Wpn-AAGun-Damage03", "R-Wpn-AAGun-ROF02", "R-Wpn-AAGun-Accuracy01"
 ];
 
 //Play video about civilians being captured by the Collective. Triggered

7e0d38a0d0fc97fd5f7107abb80e93b62ecc6219
alfred007 <highlander1599@gmail.com>
2020-09-08 18:54:10 +0200

    Updating Collective research

--- script/campaign/cam2-c.js
+++ script/campaign/cam2-c.js
@@ -11,7 +11,7 @@ const COLLECTIVE_RES = [
 	"R-Vehicle-Engine04", "R-Vehicle-Metals05", "R-Cyborg-Metals05",
 	"R-Wpn-Cannon-Accuracy02", "R-Wpn-Cannon-Damage05","R-Wpn-Cannon-ROF01",
 	"R-Wpn-Flamer-Damage06", "R-Wpn-Flamer-ROF03", "R-Wpn-MG-Damage06",
-	"R-Wpn-MG-ROF02", "R-Wpn-Mortar-Acc02", "R-Wpn-Mortar-Damage03",
+	"R-Wpn-MG-ROF02", "R-Wpn-Mortar-Acc02", "R-Wpn-Mortar-Damage05",
 	"R-Wpn-Mortar-ROF02", "R-Wpn-Rocket-Accuracy02", "R-Wpn-Rocket-Damage05",
 	"R-Wpn-Rocket-ROF03", "R-Wpn-RocketSlow-Accuracy03", "R-Wpn-RocketSlow-Damage05",
 	"R-Sys-Sensor-Upgrade01", "R-Wpn-RocketSlow-ROF01", "R-Wpn-Howitzer-ROF01",

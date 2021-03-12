ac596ec10e7a97cbab35be02539cc47512f0792f
highlander1599 <highlander1599@gmail.com>
2021-03-12 14:36:59 +0100

    Remove one damage upgrade for NP

--- script/campaign/cam1-4a.js
+++ script/campaign/cam1-4a.js
@@ -7,7 +7,7 @@ const NEW_PARADIGM_RES = [
 	"R-Sys-Engineering01", "R-Wpn-MG-Damage04", "R-Wpn-MG-ROF01", "R-Wpn-Cannon-Damage03",
 	"R-Wpn-Flamer-Damage03", "R-Wpn-Flamer-Range01", "R-Wpn-Flamer-ROF01",
 	"R-Defense-WallUpgrade03","R-Struc-Materials03", "R-Vehicle-Engine02",
-	"R-Struc-RprFac-Upgrade03", "R-Wpn-Rocket-Damage03", "R-Wpn-Rocket-ROF03",
+	"R-Struc-RprFac-Upgrade03", "R-Wpn-Rocket-Damage02", "R-Wpn-Rocket-ROF03",
 	"R-Vehicle-Metals02", "R-Wpn-Mortar-Damage03", "R-Wpn-Rocket-Accuracy02",
 	"R-Wpn-RocketSlow-Damage02", "R-Wpn-Mortar-ROF01",
 ];

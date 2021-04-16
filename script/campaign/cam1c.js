4d3894c72a96ce4f3221b4db9b3fdcd05a9c11eb
highlander1599 <highlander1599@gmail.com>
2021-04-16 23:38:52 +0200

    Alpha 06 tweaks
    Adjusts the NP factory production speed and moves the rally point of the southern Scav factory to a better point.

--- script/campaign/cam1c.js
+++ script/campaign/cam1c.js
@@ -299,7 +299,7 @@ function eventStartLevel()
 			assembly: "NPCentralFactoryAssembly",
 			order: CAM_ORDER_ATTACK,
 			groupSize: 4,
-			throttle: camChangeOnDiff(camSecondsToMilliseconds(45)),
+			throttle: camChangeOnDiff(camSecondsToMilliseconds(50)),
 			data: {
 				regroup: false,
 				repair: 40,
@@ -311,7 +311,7 @@ function eventStartLevel()
 			assembly: "NPNorthFactoryAssembly",
 			order: CAM_ORDER_ATTACK,
 			groupSize: 4,
-			throttle: camChangeOnDiff(camSecondsToMilliseconds(30)),
+			throttle: camChangeOnDiff(camSecondsToMilliseconds(40)),
 			data: {
 				regroup: false,
 				repair: 66,
